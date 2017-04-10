b8460f214e7cd5d4fcfb9bc34d8c2045df949239
Logan McAnsh <logan@mcan.sh>
2017-04-10 16:06:28 -0400

    add signup form, css
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,28 @@
 require "./config/environment"
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'supersecretsession'
+  end
+
+  get '/' do
+    erb :root, :layout => :layout
+  end
+
+  get '/signup' do
+    erb :'account/signup', :layout => :layout
+  end
+
+  post '/signup' do
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect to '/'
+    else
+      redirect to 'signup'
+    end
+  end
+end
--- config.ru
+++ config.ru
@@ -3,12 +3,10 @@ require 'sass/plugin/rack'
 Sass::Plugin.options[:style] = :compressed
 use Sass::Plugin::Rack
 
-use Rack::Static, :urls => ['/stylesheets'], :root => 'public' # Rack fix allows seeing the css folder.
+use Rack::Static, urls: ['/stylesheets'], root: 'public'
 
 if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending run `rake db:migrate` to resolve the issue.'
 end
 
-use TaskController
-use UserController
 run ApplicationController
